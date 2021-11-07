4295ff30f0c1448b8608e8db645386c8ac683e91
Marcos Oliveira <marcos-oliveiragv@hotmail.com>
2021-11-07 11:44:04 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,7 +24,8 @@ const DonutChart = () => {
             });
     }, []);
 
-    /* const mockData = {
+    /* dados estaticos
+    const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     } */
